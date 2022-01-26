c6fd435cc825fc5d24b3c0c745c67be6bebacdef
vinayaachar <vinayaachar@gmail.com>
2022-01-25 16:51:23 -0800

    Add about and home page descriptions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+
+
 
 function App() {
   return (
@@ -8,6 +13,12 @@ function App() {
       <div className='flex flex-col justify-between-screen'>
         <Navbar />
         <main className='container mx-auto px-3 pb-12'>Content</main>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/notfound' element={<NotFound />}/>
+          <Route path='/*' element={<NotFound />}/>
+        </Routes>
         <Footer />
       </div>
     </Router>
